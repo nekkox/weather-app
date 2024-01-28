01be702f8ecb5ca5c39ffebf760772edbe1a2c9b
nekkox <108139374+nekkox@users.noreply.github.com>
2024-01-28 19:46:35 +0000

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import GetLocation from './components/GetLocation.vue'
+
+</script>
+
+<template>
+ <GetLocation />
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/GetLocation.vue
@@ -0,0 +1,32 @@
+<script setup>
+    import { ref, onMounted } from "vue";
+    const coords = ref();
+    const geolocationBlockedByUser = ref(false);
+    const getGeolocation = () => {
+        return new Promise((resolve, reject)=>{
+            navigator.geolocation.getCurrentPosition(
+                (position)=>{
+                    coords.value = {
+                        latitude: position.coords.latitude,
+                        longitude: position.coords.longitude
+                    }
+                    resolve('working');
+                },
+                (error)=>{
+                    geolocationBlockedByUser.value = true;
+                    reject(error);
+                }
+            );
+        });
+    }
+
+    onMounted(async ()=>{
+        const getLocation = await getGeolocation()
+        console.log(getLocation);
+    })
+</script>
+
+<template>
+
+
+</template>
